d54aaa9ccdd229758faad0b91493521e35e97dc0
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-10-09 18:08:19 +0530

    using methods and classes

--- com/bridgelabz/EmployeewageComputation.java
+++ com/bridgelabz/EmployeewageComputation.java
@@ -3,42 +3,47 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class EmployeewageComputation {
-	 public static final int IS_FULLTIME = 1;
-	    public static final int IS_PARTTIME = 2;
-	    public static final int Emp_Rate_PerHr = 20;
-	    public static final int NO_WORKINGDAYS = 20;
-	    public static final int MAX_HRS_MONTH = 100;
+	public static final int IS_FULLTIME = 1;
+    public static final int IS_PARTTIME = 2;
+    public static final int Emp_Rate_PerHr = 20;
+    public static final int NO_WORKINGDAYS = 20;
+    public static final int MAX_HRS_MONTH = 100;
 
-	    public static void main(String[] args) {
-	        System.out.println("Welcome To Employee Wage Computation Program");
-	        
-	        int empHrs = 0;
-	        int empWage = 0;
-	        int totalEmpWage = 0;
-	        int totalEmpHrs = 0;
-	        int totalWorkingDays = 0;
-	        
-	        while (totalEmpHrs <= MAX_HRS_MONTH && totalWorkingDays < NO_WORKINGDAYS) {
-	            totalWorkingDays++;
-	            Random random = new Random();
-	            int empCheck = random.nextInt(3);
-	            switch (empCheck) {
-	                case IS_FULLTIME: // FullTime Employee
-	                    empHrs = 8;
-	                    break;
-	                case IS_PARTTIME: // PartTime Employee
-	                    empHrs = 4;
-	                    break;
-	                default: // Employee is absent
-	                    empHrs = 0;
-	            }
-	            empWage = Emp_Rate_PerHr * empHrs;
-	            totalEmpHrs += empHrs;
-	            System.out.println("Day : " + totalWorkingDays + " : Employee worked : " + empHrs + " Hours " +
-	                               ", Employee Wage : " + empWage);
-	        }
-	        totalEmpWage = totalEmpHrs * Emp_Rate_PerHr;
-	        System.out.println("Total Wages of an Employee is : " + totalEmpWage);
-	    }
-	}
+    public static int computeWage() {
+      
+        int empHrs = 0;
+        int empWage = 0;
+        int totalEmpWage = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
+        
+        while (totalEmpHrs <= MAX_HRS_MONTH && totalWorkingDays < NO_WORKINGDAYS) {
+            totalWorkingDays++;
+            Random random = new Random();
+            int empCheck = random.nextInt(3);
+            switch (empCheck) {
+                case IS_FULLTIME: // FullTime Employee
+                    empHrs = 8;
+                    break;
+                case IS_PARTTIME: // PartTime Employee
+                    empHrs = 4;
+                    break;
+                default:  // Employee is absent
+                    empHrs = 0;
+            }
+            empWage = Emp_Rate_PerHr * empHrs;
+            totalEmpHrs += empHrs;
+            System.out.println("Day : " + totalWorkingDays + " : Employee worked : " + empHrs + " Hours " +
+                                ", Employee Wage : " + empWage);
+        }
+        totalEmpWage = totalEmpHrs * Emp_Rate_PerHr;
+        System.out.println("Total Wages is : " + totalEmpWage);
+        return totalEmpWage;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Employee Wage Computation Program");
+        computeWage();
+    }
+}
                     
