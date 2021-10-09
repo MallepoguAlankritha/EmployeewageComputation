a9c6986f409e56f1e5e03c1aacc9c117832caf9f
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-10-09 17:48:37 +0530

    daily wage of employee

--- com/bridgelabz/EmployeewageComputation.java
+++ com/bridgelabz/EmployeewageComputation.java
@@ -1,12 +1,25 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class EmployeewageComputation {
-	public static void main(String[] args){
-		  int IS_FULL_TIME = 1;
-		  double empCheck = Math.floor(Math.random() * 10) % 2;
-		   if(empCheck == IS_FULL_TIME)
-		System.out.println("Employee is present");
-		else
-		System.out.println("Employee is Absent");
-		}
+public static void main(String[] args) {
+	        System.out.println("Welcome To Employee Wage Computation Program");
+	        
+	        int IS_FULLTIME = 1;
+	        int Emp_Rate_PerHr = 20;
+	        
+	        int empHrs = 0;
+	        int empWage = 0;
+	        
+	        Random random = new Random();
+	        int empCheck = random.nextInt(2);
+	        if (empCheck == IS_FULLTIME)
+	            empHrs = 8;
+	        else
+	            empHrs = 0;
+	       
+	        empWage = Emp_Rate_PerHr * empHrs;
+	        System.out.println("The daily wage of an employee is : " + empWage);
+	    }
 	}
