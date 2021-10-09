764a1d28be31c4faccb063d2ed13646209d495e2
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-10-09 17:36:02 +0530

    Employeewagecomputation

---
+++ com/bridgelabz/EmployeewageComputation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class EmployeewageComputation {
+
+	public static void main(String[] args) {
+	System.out.println("welcome to Employeewage computation ");
+
+	}
+
+}
